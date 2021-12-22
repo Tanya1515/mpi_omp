6f61a7f8161c690f0b7e2aeb6eab253f6be28d5f
Tanya <tanya1515@ispras.ru>
2021-12-22 23:27:40 +0300

    Added process death while matrix computing

--- task_2/task_2.c
+++ task_2/task_2.c
@@ -5,12 +5,42 @@
 #include <mpi-ext.h>
 #include <signal.h>
 
+#define KILL_PROC 2
+int KILL = 0;
 MPI_Comm mpi_comm;
+size_t startIdx;
+size_t step;
+int nRows = 2;
+int alpha = 3;
+int betta = 4;
 
-void MultMatrix(int *a, int *b, int *c, int *cMpi, size_t nRows, size_t startIdx, size_t step, int alpha, int betta)
+int* a = NULL;
+int* b = NULL;
+int* c = NULL;
+int* cMpi = NULL;
+
+
+void printMatrix(int *c, int n)
+{
+    printf("Begining of printing\n");
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            printf("%d\n", c[i * n + j]);
+        }
+    }
+    printf("End of printing\n");
+}
+
+
+void MultMatrix(int *a, int *b, int *c, int *cMpi, size_t nRows, size_t startIdx, size_t step, int alpha, int betta, int rank)
 {
     size_t i, j, k;
     int sum;
+    if ((rank == KILL_PROC) && (KILL == 0))
+    {
+        raise(SIGKILL);
+        KILL = 1;
+    }
     for (i = 0; i < nRows; i++) {
         for (j = startIdx; j < nRows && j < startIdx + step; j++) {
             sum = 0;
@@ -22,14 +52,15 @@ void MultMatrix(int *a, int *b, int *c, int *cMpi, size_t nRows, size_t startIdx
     }
 }
 
-void printMatrix(int *c, int n)
+
+int min (int num1, int num2)
 {
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-            printf("%d", c[i * n + j]);
-        }
-    }
+    if (num1 > num2)
+        return num2;
+    else
+        return num1;
 }
+
 
 void ElemMatrix(int *c, size_t n, int isRand)
 {
@@ -44,30 +75,37 @@ void ElemMatrix(int *c, size_t n, int isRand)
     }
 }
 
+
 static void verbose_errhandler(MPI_Comm* comm, int* err, ...) {
     int rank, size, amount_f, len;
+    int old_size;
+    int old_rank;
     char errstr[MPI_MAX_ERROR_STRING];
     
     MPI_Group group_f;
-    MPI_Comm_size(mpi_comm, &size);
+    MPI_Comm_size(mpi_comm, &old_size);
+    MPI_Comm_size(mpi_comm, &old_rank);
+    //printf ("Amount of processes in communicator with failed processes: %d", size);
     
     MPIX_Comm_failure_ack(mpi_comm);
     MPIX_Comm_failure_get_acked(mpi_comm, &group_f);
     MPI_Group_size(group_f, &amount_f);
     MPI_Error_string( *err, errstr, &len );
+    //printf("Rank %d / %d: Notified of error %s in %d processes\n", rank, size, errstr, amount_f);
     
     MPIX_Comm_shrink(*comm, &mpi_comm);
 
     MPI_Comm_rank(mpi_comm, &rank);
     MPI_Comm_size(mpi_comm, &size);
-}
-
-int min (int num1, int num2)
-{
-    if (num1 > num2)
-        return num2;
-    else 
-        return num1;
+    //printf ("Amount of processes in communicator with failed processes: %d", size);
+    MPI_Barrier(mpi_comm);
+    
+    if (old_rank == 0)
+    {
+        startIdx = (nRows / old_size * KILL_PROC) + min (KILL_PROC, (nRows % old_size));
+        step = nRows / old_size + (rank < (nRows % old_size));
+        MultMatrix(a, b, c, cMpi, nRows, startIdx, step, alpha, betta, rank);
+    }
 }
 
 int main(int argc, char *argv[])
@@ -75,35 +113,28 @@ int main(int argc, char *argv[])
 
     MPI_Init(&argc, &argv);
 
-        for (int nRows = 64; nRows <= 128; nRows *= 2) {
-
             int nProcs, rank;
             MPI_Comm_size(MPI_COMM_WORLD, &nProcs);
             MPI_Comm_rank(MPI_COMM_WORLD, &rank);
             mpi_comm = MPI_COMM_WORLD;
 
-            int *a = malloc(sizeof(int)*nRows*nRows);
-            int *b = malloc(sizeof(int)*nRows*nRows);
-            int *c = malloc(sizeof(int)*nRows*nRows);
+            a = malloc(sizeof(int)*nRows*nRows);
+            b = malloc(sizeof(int)*nRows*nRows);
+            c = malloc(sizeof(int)*nRows*nRows);
             int *cMpiRes = malloc(sizeof(int)*nRows*nRows);
-            int alpha = 3;
-            int betta = 4;
+            cMpi = malloc(sizeof(int)*nRows*nRows);
 
             MPI_Errhandler errh;
 
             MPI_Comm_create_errhandler(verbose_errhandler, &errh);
             MPI_Comm_set_errhandler(MPI_COMM_WORLD, errh);
-
-            if (rank == 2) {
-                raise(SIGKILL);
-            }
             
-            MPI_Barrier(mpi_comm);
             if (!rank) {
                 ElemMatrix(a, nRows, 1);
                 ElemMatrix(b, nRows, 1);
                 ElemMatrix(c, nRows, 1);
                 ElemMatrix(cMpiRes, nRows, 0);
+                ElemMatrix(cMpi, nRows, 0);
             }
             MPI_Comm_size(mpi_comm, &nProcs);
             MPI_Bcast(a, nRows * nRows, MPI_INT, 0, mpi_comm);
@@ -111,23 +142,27 @@ int main(int argc, char *argv[])
 
             double timerMpi;
 
-            size_t startIdx = (nRows / nProcs * rank) + min (rank, (nRows % nProcs));
-            size_t step = nRows / nProcs + (rank < (nRows % nProcs));
-            int *cMpi = malloc(sizeof(int)*nRows*nRows);
-            ElemMatrix(cMpi, nRows, 0);
+            startIdx = (nRows / nProcs * rank) + min (rank, (nRows % nProcs));
+            step = nRows / nProcs + (rank < (nRows % nProcs));
             MPI_Barrier(mpi_comm);
             timerMpi = MPI_Wtime();
-            MultMatrix(a, b, c, cMpi, nRows, startIdx, step, alpha, betta);
+            MultMatrix(a, b, c, cMpi, nRows, startIdx, step, alpha, betta, rank);
             MPI_Barrier(mpi_comm);
             timerMpi = MPI_Wtime() - timerMpi;
             MPI_Barrier(mpi_comm);
 
             MPI_Reduce(cMpi, cMpiRes, nRows * nRows, MPI_INT, MPI_SUM, 0, mpi_comm);
 
-            if (!rank) {
+            /*if (!rank) {
                 printf("Size of matrix: %d \n", nRows);
                 printf("Amount of processes, that are alive: %d \n", nProcs);
                 printf("Time for matrix computing: %f \n", timerMpi);
+            }*/
+            
+            if (rank == 0)
+            {
+                printf("Final matrix: \n");
+                printMatrix(cMpiRes, nRows);
             }
 
             free(a);
@@ -135,7 +170,6 @@ int main(int argc, char *argv[])
             free(c);
             free(cMpi);
             free(cMpiRes);
-        }
     MPI_Finalize();
     return 0;
 }
