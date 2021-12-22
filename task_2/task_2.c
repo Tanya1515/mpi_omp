18279daf3eb13a31b68303d52017290a6e0b4474
Tanya <tanya1515@ispras.ru>
2021-12-22 19:49:11 +0300

    Working programm

--- task_2/task_2.c
+++ task_2/task_2.c
@@ -5,6 +5,8 @@
 #include <mpi-ext.h>
 #include <signal.h>
 
+MPI_Comm mpi_comm;
+
 void MultMatrix(int *a, int *b, int *c, int *cMpi, size_t nRows, size_t startIdx, size_t step, int alpha, int betta)
 {
     size_t i, j, k;
@@ -42,31 +44,23 @@ void ElemMatrix(int *c, size_t n, int isRand)
     }
 }
 
-/*static void verbose_errhandler(MPI_Comm* comm, int* err, ...) {
-    MPI_Comm mpi_comm = *comm;
+static void verbose_errhandler(MPI_Comm* comm, int* err, ...) {
     int rank, size, amount_f, len;
     char errstr[MPI_MAX_ERROR_STRING];
+    
     MPI_Group group_f;
-
-    MPI_Comm_rank(mpi_comm, &rank);
     MPI_Comm_size(mpi_comm, &size);
-    printf("Size of communicator: %d\n", size);
-
+    
     MPIX_Comm_failure_ack(mpi_comm);
     MPIX_Comm_failure_get_acked(mpi_comm, &group_f);
     MPI_Group_size(group_f, &amount_f);
-
     MPI_Error_string( *err, errstr, &len );
-    printf("Rank %d / %d: Notified of error %s in %d processes\n",
-           rank, size, errstr, amount_f);
-    MPIX_Comm_shrink(mpi_comm, &mpi_comm);
+    
+    MPIX_Comm_shrink(*comm, &mpi_comm);
 
-    MPI_Barrier(mpi_comm);
-
+    MPI_Comm_rank(mpi_comm, &rank);
     MPI_Comm_size(mpi_comm, &size);
-    printf("Size of communicator without failed process: %d\n", size);
-
-}*/
+}
 
 int min (int num1, int num2)
 {
@@ -86,6 +80,7 @@ int main(int argc, char *argv[])
             int nProcs, rank;
             MPI_Comm_size(MPI_COMM_WORLD, &nProcs);
             MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+            mpi_comm = MPI_COMM_WORLD;
 
             int *a = malloc(sizeof(int)*nRows*nRows);
             int *b = malloc(sizeof(int)*nRows*nRows);
@@ -94,23 +89,25 @@ int main(int argc, char *argv[])
             int alpha = 3;
             int betta = 4;
 
-            //MPI_Errhandler errh;
+            MPI_Errhandler errh;
 
-            //MPI_Comm_create_errhandler(verbose_errhandler, &errh);
-            //MPI_Comm_set_errhandler(MPI_COMM_WORLD, errh);
+            MPI_Comm_create_errhandler(verbose_errhandler, &errh);
+            MPI_Comm_set_errhandler(MPI_COMM_WORLD, errh);
 
-            //if (rank == 2) {
-            //    raise(SIGKILL);
-            //}
-
+            if (rank == 2) {
+                raise(SIGKILL);
+            }
+            
+            MPI_Barrier(mpi_comm);
             if (!rank) {
                 ElemMatrix(a, nRows, 1);
                 ElemMatrix(b, nRows, 1);
                 ElemMatrix(c, nRows, 1);
                 ElemMatrix(cMpiRes, nRows, 0);
             }
-            MPI_Bcast(a, nRows * nRows, MPI_INT, 0, MPI_COMM_WORLD);
-            MPI_Bcast(b, nRows * nRows, MPI_INT, 0, MPI_COMM_WORLD);
+            MPI_Comm_size(mpi_comm, &nProcs);
+            MPI_Bcast(a, nRows * nRows, MPI_INT, 0, mpi_comm);
+            MPI_Bcast(b, nRows * nRows, MPI_INT, 0, mpi_comm);
 
             double timerMpi;
 
@@ -118,19 +115,19 @@ int main(int argc, char *argv[])
             size_t step = nRows / nProcs + (rank < (nRows % nProcs));
             int *cMpi = malloc(sizeof(int)*nRows*nRows);
             ElemMatrix(cMpi, nRows, 0);
-            MPI_Barrier(MPI_COMM_WORLD);
+            MPI_Barrier(mpi_comm);
             timerMpi = MPI_Wtime();
             MultMatrix(a, b, c, cMpi, nRows, startIdx, step, alpha, betta);
-            MPI_Barrier(MPI_COMM_WORLD);
+            MPI_Barrier(mpi_comm);
             timerMpi = MPI_Wtime() - timerMpi;
-            MPI_Barrier(MPI_COMM_WORLD);
+            MPI_Barrier(mpi_comm);
 
-            MPI_Reduce(cMpi, cMpiRes, nRows * nRows, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
+            MPI_Reduce(cMpi, cMpiRes, nRows * nRows, MPI_INT, MPI_SUM, 0, mpi_comm);
 
             if (!rank) {
-                printf("nRows: %d \n", nRows);
-                printf("nProcs: %d \n", nProcs);
-                printf("timer: %f \n", timerMpi);
+                printf("Size of matrix: %d \n", nRows);
+                printf("Amount of processes, that are alive: %d \n", nProcs);
+                printf("Time for matrix computing: %f \n", timerMpi);
             }
 
             free(a);
@@ -142,6 +139,3 @@ int main(int argc, char *argv[])
     MPI_Finalize();
     return 0;
 }
-
-
-
